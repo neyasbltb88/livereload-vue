f45cf96d1098323a9fa2c768d9591d3de10487f5
Neyasbltb <neyasbltb_88@mail.ru>
2019-07-05 17:03:13 +0300

    mode: 'abstract' по умолчанию

--- app/router.js
+++ app/router.js
@@ -5,7 +5,11 @@ import routes from './routes'
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-    mode: 'history',
+    // history работает, но может нарушить работу сообщений в VK.
+    // Если перезагрузить страницу с адресом, включающим роут, то сообщения отправляются,
+    // но не отображаются, новые входящие сообщения тоже могут не появляться.
+    // mode: 'history',
+    mode: 'abstract',
     routes
 })
 
